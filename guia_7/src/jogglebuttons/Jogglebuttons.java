b7755efa7800cd64d13d45c350f05cb933eff8ca
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-23 19:54:19 -0500

    ToggleButtons

---
+++ guia_7/src/jogglebuttons/Jogglebuttons.java
@@ -0,0 +1,22 @@
+
+package jogglebuttons;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Jogglebuttons {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal ventana = new Principal(); 
+        
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+        
+    }
+    
+}
---
+++ guia_7/src/jogglebuttons/tbtnInstalacion.java
@@ -0,0 +1,14 @@
+
+package jogglebuttons;
+
+/**
+ *
+ * @author isac pacheco
+ */
+class tbtnInstalacion {
+
+    static void setSelected(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
